e89eb3761ae5898471fcfb578dcd6b10603fc54c
Christian Valderrama <valderrama@valderrama.local>
2021-05-06 18:43:12 -0500

    3bh first

---
+++ src/components/homepage/homepage.component.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './homepage.styles.scss';
+
+const HomePage = () => (
+    <div className="homepage">
+        <div className="menu"> 
+            <div>
+                LOGO
+            </div>
+            <div>
+                MENU
+            </div>
+            <div>
+                REDES
+            </div>
+            <div>
+                MUTE
+            </div>
+            <div>
+                IDIOMAS
+            </div>
+        </div>
+        <div className="hero">
+            <div className="controlsVideo">
+
+            </div>
+            <div className="video">
+
+            </div>
+        </div>
+{/* 
+        <Proyectos />
+        <Descanso />
+        <Video />
+        <RedInstagram />
+        <Parners />
+        <Footer /> */}
+
+    </div>
+)
+
+export default HomePage;
